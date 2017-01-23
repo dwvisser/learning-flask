75b01b93ce5defe5b6fdb822574f1f6a89153e9a
Dale Visser <dale.visser@live.com>
2017-01-23 10:38:02 -0500

    Complete through section 5.

--- routes.py
+++ routes.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+                        'postgresql://dale:dale@localhost:5432/learningflask'
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -10,6 +18,48 @@ def index():
 def about():
   return render_template("about.html")
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+  form = SignupForm()
+  if request.method == 'POST':
+    if form.validate():
+      newuser = User(form.first_name.data, form.last_name.data,
+                     form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      session['email'] = newuser.email
+      return redirect(url_for('home'))
+    else:
+      return render_template('signup.html', form=form)
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
+
+@app.route("/home")
+def home():
+  return render_template('home.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  form = LoginForm()
+  if request.method == "POST":
+    if form.validate():
+      email = form.email.data
+      password = form.password.data
+      user = User.query.filter_by(email=email).first()
+      if user is not None and user.check_password(password):
+        session['email'] = email
+        return redirect(url_for('home'))
+      else:
+        return redirect(url_for('login'))
+    else:
+      return render_template("login.html", form=form)
+  elif request.method == "GET":
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+  session.pop('email', None)
+  return redirect(url_for('index'))
 
 if __name__ == "__main__":
   app.run(debug=True)
